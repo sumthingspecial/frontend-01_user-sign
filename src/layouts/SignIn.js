60e5bed8291f6d05ffbb23875c3046f49e22e2f2
양희영 <aa87565115@gmail.com>
2023-09-07 23:27:15 +0900

    feat: add password

--- src/layouts/SignIn.js
+++ src/layouts/SignIn.js
@@ -1,14 +1,25 @@
+import { FormControl } from "@mui/base";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
   Button,
   Card,
   Container,
   Grid,
+  IconButton,
   Input,
-  TextField,
+  InputAdornment,
+  InputLabel,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 
 function SignIn() {
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
+  const togglePasswordShow = () => {
+    setIsPasswordShown(!isPasswordShown);
+  };
+
   return (
     <Container>
       <Grid
@@ -29,10 +40,37 @@ function SignIn() {
               <Typography variant="h2">PROJECT</Typography>
             </Grid>
             <Grid item marginBottom={3} paddingX={2}>
-              <Input fullWidth placeholder="이메일" />
+              <FormControl variant="standard">
+                <InputLabel htmlFor="sign-in-email">Email</InputLabel>
+                <Input
+                  id="sign-in-email"
+                  fullWidth
+                  label="Email"
+                  placeholder="이메일"
+                />
+              </FormControl>
             </Grid>
             <Grid item marginBottom={5} paddingX={2}>
-              <Input fullWidth placeholder="비밀번호" />
+              <FormControl variant="standard">
+                <InputLabel htmlFor="sign-in-password">Password</InputLabel>
+                <Input
+                  id="sign-in-password"
+                  fullWidth
+                  label="Password"
+                  placeholder="비밀번호"
+                  type={isPasswordShown ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={togglePasswordShow}
+                      >
+                        {isPasswordShown ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
             </Grid>
             <Grid
               item
