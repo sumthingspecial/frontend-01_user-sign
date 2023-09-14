c5de71036297080c21d9dacef0b81b581fb81761
양희영 <aa87565115@gmail.com>
2023-09-14 23:53:25 +0900

    feat: form add on progress

--- src/layouts/SignIn.js
+++ src/layouts/SignIn.js
@@ -4,6 +4,7 @@ import {
   Button,
   Card,
   Container,
+  FormGroup,
   Grid,
   IconButton,
   Input,
@@ -20,6 +21,10 @@ function SignIn() {
     setIsPasswordShown(!isPasswordShown);
   };
 
+  const signInHandler = (e) => {
+    console.log(e)
+  }
+
   return (
     <Container>
       <Grid
@@ -29,61 +34,63 @@ function SignIn() {
         alignItems="center"
       >
         <Card>
-          <Grid container padding={3} flexDirection="column">
-            <Grid
-              item
-              container
-              justifyContent="center"
-              marginBottom={2}
-              padding={2}
-            >
-              <Typography variant="h2">PROJECT</Typography>
+          <form onSubmit={signInHandler}>
+            <Grid container padding={3} flexDirection="column">
+              <Grid
+                item
+                container
+                justifyContent="center"
+                marginBottom={2}
+                padding={2}
+              >
+                <Typography variant="h2">PROJECT</Typography>
+              </Grid>
+              <Grid item marginBottom={3} paddingX={2}>
+                <FormControl variant="standard" required>
+                  <InputLabel htmlFor="sign-in-email">Email</InputLabel>
+                  <Input
+                    id="sign-in-email"
+                    fullWidth
+                    label="Email"
+                    placeholder="이메일"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item marginBottom={5} paddingX={2}>
+                <FormControl variant="standard" required>
+                  <InputLabel htmlFor="sign-in-password">Password</InputLabel>
+                  <Input
+                    id="sign-in-password"
+                    fullWidth
+                    label="Password"
+                    placeholder="비밀번호"
+                    type={isPasswordShown ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={togglePasswordShow}
+                        >
+                          {isPasswordShown ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </Grid>
+              <Grid
+                item
+                container
+                justifyContent="center"
+                paddingY={1}
+                paddingX={2}
+              >
+                <Button variant="contained" type="submit" fullWidth>
+                  로그인
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item marginBottom={3} paddingX={2}>
-              <FormControl variant="standard">
-                <InputLabel htmlFor="sign-in-email">Email</InputLabel>
-                <Input
-                  id="sign-in-email"
-                  fullWidth
-                  label="Email"
-                  placeholder="이메일"
-                />
-              </FormControl>
-            </Grid>
-            <Grid item marginBottom={5} paddingX={2}>
-              <FormControl variant="standard">
-                <InputLabel htmlFor="sign-in-password">Password</InputLabel>
-                <Input
-                  id="sign-in-password"
-                  fullWidth
-                  label="Password"
-                  placeholder="비밀번호"
-                  type={isPasswordShown ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={togglePasswordShow}
-                      >
-                        {isPasswordShown ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
-            </Grid>
-            <Grid
-              item
-              container
-              justifyContent="center"
-              paddingY={1}
-              paddingX={2}
-            >
-              <Button variant="contained" fullWidth>
-                로그인
-              </Button>
-            </Grid>
-          </Grid>
+          </form>
         </Card>
       </Grid>
     </Container>
